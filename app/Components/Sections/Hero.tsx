79f32e645b953986ca79e6b8ea79e16cca8c072f
Dominik Czekański <dominik@czekanski.dev>
2025-10-23 02:37:59 +0200

    Created Navbar

---
+++ app/Components/Sections/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className='min-h-screen bg-linear-to-b from-[#0F172A] to-[#09090B] flex items-center justify-center'>
+      <h1>Hello I'm Dominik!</h1>
+    </div>
+  );
+}
