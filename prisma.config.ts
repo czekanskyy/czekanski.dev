294a0af32247cb354f1df3dc6510a1d5d6e76823
czekanskyy <czekanski.dominik@proton.me>
2026-02-18 15:30:03 +0100

    fix(prisma): move urls to prisma.config.ts and suppress ts error

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,5 +5,7 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
     url: process.env.DATABASE_URL ?? '',
+    // @ts-expect-error: directUrl is required for Prisma 7 but missing in type
+    directUrl: process.env.DIRECT_URL ?? '',
   },
 });
