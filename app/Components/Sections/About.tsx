d7a5cde0293c836aa5c79e160f3bdfc4c1aa82d3
czekanskyy <czekanski.dominik@proton.me>
2026-02-18 18:13:01 +0100

    fixed responsivity issues

--- app/Components/Sections/About.tsx
+++ app/Components/Sections/About.tsx
@@ -44,7 +44,7 @@ export default function About({ data, id, index }: AboutProps) {
         <div className='hidden md:block md:col-span-5 lg:col-span-4 xl:col-span-3 relative w-full aspect-3/4 max-w-md mx-auto bg-neutral-900 rounded-2xl overflow-hidden shadow-lg border-2 border-white/10 shadow-white/25 sepia-[0.25]'>
           <Image src={content.profileImage || '/profile.jpg'} alt='A very cool photo of me piloting an aircraft 😎' fill className='object-cover' />
         </div>
-        <div className='md:col-span-7 lg:col-span-8 xl:col-span-9 flex flex-col gap-6 text-justify text-neutral-400 text-lg leading-relaxed'>
+        <div className='md:col-span-7 lg:col-span-8 xl:col-span-9 flex flex-col gap-6 text-justify text-neutral-400 text-lg leading-relaxed break-words max-w-full [&_img]:max-w-full [&_img]:h-auto [&_pre]:whitespace-pre-wrap [&_pre]:break-all'>
           <div dangerouslySetInnerHTML={{ __html: content.bio }} />
         </div>
       </div>
